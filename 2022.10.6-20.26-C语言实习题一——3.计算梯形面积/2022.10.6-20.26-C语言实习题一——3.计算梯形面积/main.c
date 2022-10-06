d600ee27806c77e23c52abfcd8ece09afaea314f
Nwafu_LH <2385883104@qq.com>
2022-10-06 20:44:34 +0800

    2022.10.6-22.44-commit

---
+++ 2022.10.6-20.26-C语言实习题一——3.计算梯形面积/2022.10.6-20.26-C语言实习题一——3.计算梯形面积/main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+	float supline, dowline, high, area;
+
+	scanf("%f", &supline);
+	scanf("%f", &dowline);
+	scanf("%f", &high);
+	area = ((supline + dowline) * high) / 2.0; printf("%f\n", area);
+
+	return 0;
+}
